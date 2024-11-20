79d4bb74b3adf694b4655a81b56beaa001372288
Hardik Upadhyay <119848774+HardikUpadhyay007@users.noreply.github.com>
2024-11-20 19:53:12 +0530

    minor asset changes

--- src/App.js
+++ src/App.js
@@ -1,51 +1,3 @@
-// import "./App.css";
-// import Navbar from "./Components/Navbar/Navbar";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Shop from "./Pages/Shop";
-// import ShopCategory from "./Pages/ShopCategory";
-// import Product from "./Pages/Product";
-// import Cart from "./Pages/Cart";
-// import LoginSignup from "./Pages/LoginSignup";
-// function App() {
-//     return (
-//         <div>
-//             <Navbar></Navbar>
-//             <BrowserRouter>
-//                 <Routes>
-//                     <Route path="/" element={<Shop></Shop>}></Route>
-//                     <Route
-//                         path="/mens"
-//                         element={<ShopCategory category="men"></ShopCategory>}
-//                     ></Route>
-//                     <Route
-//                         path="/womens"
-//                         element={<ShopCategory category="women"></ShopCategory>}
-//                     ></Route>
-//                     <Route
-//                         path="/kids"
-//                         element={<ShopCategory category="kid"></ShopCategory>}
-//                     ></Route>
-//                     <Route
-//                         path="/product"
-//                         element={<Product></Product>}
-//                     ></Route>
-//                     <Route
-//                         path=":productId"
-//                         element={<Product></Product>}
-//                     ></Route>
-//                     <Route path="/cart" element={<Cart></Cart>}></Route>
-//                     <Route
-//                         path="/login"
-//                         element={<LoginSignup></LoginSignup>}
-//                     ></Route>
-//                 </Routes>
-//             </BrowserRouter>
-//         </div>
-//     );
-// }
-
-// export default App;
-
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -55,7 +7,7 @@ import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
 import Footer from "./Components/Footer/Footer";
-import men_banner from "./Components/Assets/banner1.png";
+import banner_1 from "./Components/Assets/banner1.png";
 
 import ServiceComponent from "./Components/ServiceComponent/ServiceComponent";
 function App() {
@@ -68,10 +20,7 @@ function App() {
                     <Route
                         path="/mens"
                         element={
-                            <ShopCategory
-                                banner={men_banner}
-                                category="parts"
-                            />
+                            <ShopCategory banner={banner_1} category="parts" />
                         }
                     />
                     <Route
